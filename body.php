7acfc9523046a595640beca1188cc89d20d641d9
Abayomzee <okegbenroabayo@gmail.com>
2019-02-19 17:38:02 +0100

    first Commit

---
+++ body.php
@@ -0,0 +1,20 @@
+
+<!-- Welcome message section  -->
+    <div class="typedWelcomeNote">
+      <span> Welcome to teamWewe official website </span>
+    </div>
+<!--  -->
+
+<!-- Nav menu section -->
+    <div class="navigation">
+
+      <div class="logoBox">
+
+      </div>
+
+      <div class="menus">
+
+      </div>
+
+    </div>
+<!--  -->
---
+++ footer.php
@@ -0,0 +1,3 @@
+<script src="assets/scripts/js/jQuery.min.js"></script>
+</body>
+</html>
